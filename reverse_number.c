c778a8cfbeca5b3c41abaeb84f202af3c5979b31
gateswell <caoshuhuan@yeah.net>
2019-01-17 20:18:05 +0800

    script for chapter6 Q5

---
+++ reverse_number.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(void){
+	int num,dom;
+	printf("Enter a number:");
+	scanf("%d",&num);
+	printf("The reversal id:" );
+	do{
+		dom= num%10;
+		num /=10;
+		if(dom !=0){
+			printf("%d",dom);
+		}
+		else{
+			break;
+		}
+	}while(dom!=0);
+	return 0;
+} 
